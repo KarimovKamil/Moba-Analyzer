36327f2749675337b56ac5c086e2e266c9b0fa8e
artur <antegzeperob@gmail.com>
2018-11-17 11:33:00 +0300

    change schema

--- src/main/resources/sql/init/schema.sql
+++ src/main/resources/sql/init/schema.sql
@@ -1,14 +1,7 @@
-DROP TABLE IF EXISTS spell;
 DROP TABLE IF EXISTS hero;
 DROP TABLE IF EXISTS player;
 DROP TABLE IF EXISTS match;
-
-CREATE TABLE spell (
-  spell_name  VARCHAR,
-  description VARCHAR,
-  hero_name   VARCHAR,
-  PRIMARY KEY (spell_name)
-);
+DROP TABLE IF EXISTS pick;
 
 CREATE TABLE hero (
   hero_name      VARCHAR,
@@ -41,6 +34,28 @@ CREATE TABLE player (
 
 CREATE TABLE match (
   match_id BIGSERIAL,
-  data     JSONB,
+  dire_win BOOLEAN,
+  average_rating INT,
+  start_time BIGINT,
+  match_duration BIGINT,
   PRIMARY KEY (match_id)
+);
+
+CREATE TABLE pick (
+  pick_id BIGSERIAL,
+  player_id BIGINT,
+  hero_name VARCHAR,
+  dire BOOLEAN,
+  net_worth INT,
+  gpm INT,
+  xpm INT,
+  kills INT,
+  assissts INT,
+  deaths INT,
+  total_damage INT,
+  total_heal INT,
+  total_damage_taken INT,
+  wards_placed INT,
+  team_gold INT,
+  PRIMARY KEY (pick_id)
 );
